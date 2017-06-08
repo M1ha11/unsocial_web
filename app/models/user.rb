a79f3aba27eb071f48fe620e5e88fc688af6c506
Ormand89 <komoskoav@gmail.com>
2017-06-08 13:05:42 +0300

    Picture uploader and avatar, trash views

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,16 @@
 class User < ApplicationRecord
+  mount_uploader :avatar, AvatarUploader
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :albums, dependent: :destroy
+
+  # User Avatar Validation
+  validates_integrity_of  :avatar
+  validates_processing_of :avatar
+
+  private
+
+    def avatar_size_validation
+      errors[:avatar] << "should be less than 500KB" if avatar.size > 0.5.megabytes
+    end
 end
