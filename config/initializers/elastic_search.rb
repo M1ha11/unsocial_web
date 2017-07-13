85479ba8b16b3aa70f99adfc64150c082e292be8
Ormand89 <komoskoav@gmail.com>
2017-07-13 13:26:08 +0300

    Elasticsearch initializer update

--- config/initializers/elastic_search.rb
+++ config/initializers/elastic_search.rb
@@ -1,1 +1,1 @@
-Elasticsearch::Model.client = Elasticsearch::Client.new host: "search-myelasticservice-7evfpsw7dhdsg6ddl37j5sx5ya.us-east-2.es.amazonaws.com", log: true
+Elasticsearch::Model.client = Elasticsearch::Client.new host: "search-myelasticservice-7evfpsw7dhdsg6ddl37j5sx5ya.us-east-2.es.amazonaws.com", port: 80, log: true
