69e977b35562a24fae03c3b8b921b4556a552163
Ormand89 <komoskoav@gmail.com>
2017-07-27 12:18:25 +0300

    Correct assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,7 +22,7 @@ Rails.application.configure do
   config.assets.js_compressor = :uglifier
   config.assets.css_compressor = :sass
 
-  config.public_file_server.headers = { 'Cache-Control' => 'public, max-age=14400' }
+  config.public_file_server.headers = { 'Cache-Control' => 'public, max-age=691200' }
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
