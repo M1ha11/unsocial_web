060c9ffe42cec194008c96fcc39b32cf973b74f3
Ormand89 <komoskoav@gmail.com>
2017-07-27 16:24:02 +0300

    Update view forms

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -34,9 +34,6 @@ class PictureUploader < CarrierWave::Uploader::Base
   version :thumb do
     process resize_to_fill: [400, 400]
   end
-  version :preview do
-    process resize_to_fill: [200, 200]
-  end
 
   # Create different versions of your uploaded files:
 
